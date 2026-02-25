dbf9f922a65d9bb2ac6a70d04f19e0ec001117dc
mikunino <moisesdj@uninorte.edu.co>
2026-02-25 15:30:46 +0000

    Main

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main (String[]args){
+        System.out.println("Hola elija una funcion: ");
+        int op;
+        System.out.println("Bienvenido");
+        System.out.println("Registrar clientes");
+        System.out.println("Listar clientes");
+        System.out.println("Eliminar clientes");
+        System.out.println("Listar pedidos de clientes");
+        System.out.println("Salir");
+        Scanner read = new Scanner();
+        op.read.nextInt();
+        switch(op){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Opcion invalida vuelva a escribir: ");
+                op.read.nextInt();
+        }
+    }
+}

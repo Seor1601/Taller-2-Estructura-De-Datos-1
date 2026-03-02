2855b1808974ab8fcedb9c445bbec41533feed61
mikunino <moisesdj@uninorte.edu.co>
2026-03-02 08:02:03 -0500

    Add 'id' parameter and update toString method

--- Registrar_pedido.java
+++ Registrar_pedido.java
@@ -12,18 +12,21 @@ public class Usuario {
     private int id;
     private int id_pedido;
     private int activo_p;
+    private int total;
 
-    public Usuario(int precio, String nombre, int cantidad, int id_pedido, int activo_p) {
+    public Usuario(int precio, String nombre, int cantidad, int id_pedido, int activo_p, int id) {
         this.precio = precio;
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.id_pedido = id_pedido;
+        this.id = id;
         this.total= cantidad * precio;
         this.activo_p = activo_p;
+        
     }
 
     @Override
     public String toString() {
-        return nombre + "," + cantidad + "," + precio + "," + total;
+        return id + "," + nombre + "," + cantidad + "," + id_precio+ "," + precio + "," + total + ", " + activo_p;
     }
 }
